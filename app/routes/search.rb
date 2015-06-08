2e7cfeb367f77525b083dfd8d5a058e0fda9e3c5
Patrick Farrell <mr.patfarrell@gmail.com>
2015-06-08 12:43:28 -0700

    listing id search available

--- app/routes/search.rb
+++ app/routes/search.rb
@@ -1,4 +1,26 @@
 class App < Sinatra::Application
-  get "/search" do
+  def specific_header
+    props={}
+    props["date"]={value:lambda{|x| x.date.strftime("%Y-%m-%d %H:%M:%S")}}
+    props["tags"]={value:lambda{|x| x.tags.map{|y| y.tag}.join(", ")}}
+    props
+  end
+
+  get "/search/:group" do
+    group = Group.find(name: params[:group])
+    tags = Tag.find(group: group, tag: params[:q])
+    data = tags.nil? ? nil : tags.entries.sort_by{|entry| entry.date}.reverse
+    if data.nil?
+      haml :error, locals: {msg: "No matches"}
+    else
+      haml :specific, locals: {
+        group: group.name, 
+        tag: params[:q], 
+        model: {
+          header: specific_header, 
+          data: data
+        }, 
+        base: "/entries/#{group.name}"} 
+    end
   end
 end
