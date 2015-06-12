b8b87917feb5827748d5b7c0cf217612567ab872
Patrick Farrell <mr.patfarrell@gmail.com>
2015-06-11 18:05:48 -0700

    remove download from main page

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -1,6 +1,6 @@
 class App < Sinatra::Application
   get "/" do
-    haml :index, locals: {groups: Group.all}
+    haml :index, {locals: {groups: Group.all}, layout: :layout_no_download}
   end
 
 end
